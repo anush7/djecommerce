8ed08902d8232448a17d9a043caf3c570ba38177
anush <anush.dk@gmail.com>
2016-10-17 21:40:35 +0530

    update

--- djecommerce/users/templatetags/permission_tags.py
+++ djecommerce/users/templatetags/permission_tags.py
@@ -13,17 +13,20 @@ def get_staff_nav_bar(user):
 	# 		), 
 	# 	user.groups.all()
 	# )
-	for x in user.groups.all():
-		modules.extend(
-			[per.split('access_')[1] for per in x.permissions.filter(codename__startswith='access')\
-			.values_list('codename',flat=True) if per in role_permissions]
-		)
+	if user.is_admin:
+		modules = modulesData.keys()
+	else:
+		for x in user.groups.all():
+			modules.extend(
+				[per.split('access_')[1] for per in x.permissions.filter(codename__startswith='access')\
+				.values_list('codename',flat=True) if per in role_permissions]
+			)
 
-	for i,mod in enumerate(modules):
-		if mod == 'catalogcategory':
-			modules[i] = 'category'
-		elif mod == 'productattribute':
-			modules[i] = 'attribute'
+		for i,mod in enumerate(modules):
+			if mod == 'catalogcategory':
+				modules[i] = 'category'
+			elif mod == 'productattribute':
+				modules[i] = 'attribute'
 
 	modules = {mod:modulesData[mod] for mod in modules}
 	return {'modules':modules}
