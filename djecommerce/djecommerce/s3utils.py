371dd40c68046c31d18da754b574f13bc5dfb5fa
anush <anush@doublespring.com>
2016-10-28 12:04:19 +0530

    update

--- djecommerce/djecommerce/s3utils.py
+++ djecommerce/djecommerce/s3utils.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 from django.utils.deconstruct import deconstructible
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+# StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+# MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
 
-# class StaticRootS3BotoStorage(S3BotoStorage):
-#     location = 'static'
+class StaticRootS3BotoStorage(S3BotoStorage):
+    location = 'static'
 
-# class MediaRootS3BotoStorage(S3BotoStorage):
-#     location = 'media'
+class MediaRootS3BotoStorage(S3BotoStorage):
+    location = 'media'
 
 AWS_STORAGE_BUCKET_NAME = getattr(settings, 'AWS_STORAGE_BUCKET_NAME', None)
 @deconstructible
